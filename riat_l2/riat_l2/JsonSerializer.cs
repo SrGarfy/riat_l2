a676fdf387f109bd37b3535b2aaca330f6b0f3e3
Sr.Garfy <sr.garfy@gmail.com>
2016-11-11 13:33:22 +0400

    start

---
+++ riat_l2/riat_l2/JsonSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace riat_l2
+{
+    class JsonSerializer : Serializer
+    {
+        public byte[] Serialize<T>(T obj)
+        {
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj));
+        }
+
+        public T Deserialize<T>(byte[] data)
+        {
+            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data));
+        }
+    }
+}
---
+++ riat_l2/riat_l2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace riat_l2
+{
+    class Program
+    {
+        private static readonly Serializer[] serializers = { new JsonSerializer(), new XmlSerializerMy() };
+        public static Serializer getSerializer(string type)
+        {
+            if (type != "Json" && type != "Xml")
+            {
+                throw new System.ArgumentException("Wrong type.", type);
+            }
+            return (type == "Json" ? serializers[0] : serializers[1]);
+        }
+
+        static void Main(string[] args)
+        {
+            var serializingType = Console.ReadLine();
+            var serializer = Program.getSerializer(serializingType);
+            var inputData = Encoding.UTF8.GetBytes(Console.ReadLine());
+            var input = serializer.Deserialize<Input>(inputData);
+            var output = input.createOutput();
+            Console.WriteLine(Encoding.UTF8.GetString(serializer.Serialize(output)));
+        }
+    }
+}
